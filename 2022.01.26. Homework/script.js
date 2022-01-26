68c25fb788ce8f2df23f41065507ac2340a4da11
Kostiantyn Lisetskyi <lis.kostiantyn@gmail.com>
2022-01-26 21:04:19 +0200

    Variables

---
+++ 2022.01.26. Homework/script.js
@@ -0,0 +1,58 @@
+// переменная для хранения возраста пользователя
+
+let userAge = 11;
+userAge = 12;
+console.log(userAge);
+
+// переменная для хранения имени пользователя
+
+let userName = 'Jack';
+console.log(userName);
+
+// переменная для хранения фамилии пользователя
+
+let userSurname = 'Jackson';
+console.log(userSurname);
+
+// переменная которая хранит нового пользователя
+
+let newUser = 'Poll';
+console.log(newUser);
+
+// переменная которая храненит список покупок
+
+let listShopping = 'listShopping';
+console.log(listShopping);
+
+// переменная, которая хранит информацию о том, является ли пользователь в сети (online)
+
+const isUserOnline = false;
+if (isUserOnline) {
+    console.log('Позвонить менеджеру')
+} else {
+    console.log('Отсутсвие на работе')
+}
+
+// переменная, которая хранит зарплату работника
+
+let workerSalory = 1000;
+console.log(workerSalory);
+
+// переменная, которая хранит координаты курсора (мыши)
+
+const xMouse = 'x = 150';
+const space = ' ';
+const yMouse = 'y = 200';
+
+const result = xMouse + space + yMouse;
+console.log(result);
+
+// выведение в консль стороку 'Hello World!'
+
+let helloWorld = 'Hello World!'
+console.log(helloWorld);
+
+// переменная и выведение ее в  консоль с подписью, которая будет содержать название переменной
+
+const userNet = 'Mark';
+console.log('userNet', userNet);
